14a3e50d37e93bc8eb897fbb9b3a418c754bce95
wglee0511 <ranazang@naver.com>
2024-05-22 16:01:33 +0900

    feature: login ui

--- StorybookLoginApp/ViewController.swift
+++ StorybookLoginApp/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBAction func loginButtonAction(_ sender: Any) {
+        let email = emailField.text
+        let password = passwordField.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
