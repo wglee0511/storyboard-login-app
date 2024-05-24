68908d006a3b6a8f5e238d4477626c4ad9178f57
wglee0511 <ranazang@naver.com>
2024-05-24 18:06:27 +0900

    feature: textfield delegate

--- StorybookLoginApp/ViewController.swift
+++ StorybookLoginApp/ViewController.swift
@@ -8,23 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var warningLabel: UILabel!
     @IBAction func loginButtonAction(_ sender: Any) {
-        let email = emailField.text
-        let password = passwordField.text
+        let email = emailField.text ?? ""
+        let password = passwordField.text ?? ""
         
-        guard email != nil else {
+        guard !email.isEmpty else {
             showAlert(message: "이메일을 입력해주세요")
             return
         }
         
-        guard password != nil else {
+        guard !password.isEmpty else {
             showAlert(message: "비밀번호를 입력해주세요")
             return
         }
+        
+        
+    }
+    
+    func setTextFieldInitialStyle(
+        textFieldValue: UITextField,
+        placeholder: String
+    ) {
+        textFieldValue.placeholder = placeholder
+        textFieldValue.layer.borderColor = UIColor.systemGray3.cgColor
+        textFieldValue.layer.borderWidth = 1
+        textFieldValue.layer.cornerRadius = 5
     }
     
     func showAlert(message: String){
@@ -37,9 +49,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTextFieldInitialStyle(textFieldValue: emailField, placeholder: "이메일을 입력해주세요")
+        setTextFieldInitialStyle(textFieldValue: passwordField, placeholder: "비밀번호를 입력해주세요")
+        
+        emailField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        switch textField {
+        case emailField:
+            let countText = textField.text?.count ?? 0
+            let isValid = (6 ... 12).contains(countText)
+            
+            textField.layer.borderColor = isValid ? UIColor.systemGray3.cgColor : UIColor.red.cgColor
+            textField.tintColor = .red
+            
+            return isValid
+        default:
+            return true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        switch textField {
+        case emailField:
+            passwordField.becomeFirstResponder()
+        case passwordField:
+            loginButtonAction(self)
+        default:
+            break
+        }
+        
+        return false
+    }
+}
